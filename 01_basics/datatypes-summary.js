2b5986eaf60ada2c8c053f1c10092ea0c53c2d44
Mohammed Maaz <maazamir666@gmail.com>
2025-05-14 17:38:39 +0000

    Studied the diff btw stack and heap memories

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -24,4 +24,31 @@ const functionEx = function(){
     
 }
 
-// The End..
+// The End.. (Picture Abhi Baki hai Mere Dost)
+
+// Stack VS Heap Memory
+
+// Primitive Stored in Stack (Call by value)
+// Reference Stored in Heap (Call by Reference)
+
+let userName = "Azeem Basha"
+let secondUser = userName // here we only copied the value, didnt change the original value
+secondUser = "Khavi Basha"
+
+console.log(userName);
+console.log(secondUser);
+// both will print different names
+
+let personOne = {
+    email : "azeem@google.com",
+    upi : "azeembasha01@google.com"
+}
+
+let personTwo = personOne
+personTwo.email = "khavi@google.com"
+
+console.log(personTwo.email);
+console.log(personOne.email);
+// both will print same value => both were pointing to the same object in heap **
+
+// The End()
