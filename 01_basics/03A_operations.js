013635709746b859f55954db1ea2318b6c54dd39
Mohammed Maaz <maazamir666@gmail.com>
2025-05-14 11:48:36 +0000

    studied about comparison & basic arithmetic ops

---
+++ 01_basics/03A_operations.js
@@ -0,0 +1,15 @@
+// Operators :
+// (a + b)
+// (a - b)
+// (a * b)
+// (a ** b)
+// (a / b)
+// (a % b)
+
+console.log("1" + 2); // => 12
+console.log(1 + "2"); // => 12
+console.log("1" + 2 + 2); // => 122
+console.log(1 + 2 + "2"); // => 32
+
+
+
