b04f63ebd2e7f2067a45af44a49422f2d3a3d38b
Mohammed Maaz <maazamir666@gmail.com>
2025-05-16 06:19:34 +0000

    Studied about dates in JS

---
+++ 01_basics/07_DateinJS.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let newDate = new Date(2023, 4, 12) // => Moth starts with 0 here
+console.log(newDate.toString());
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+
+let anotherDate = new Date("2023-4-14") // => Here month starts with 1
+console.log(anotherDate.toString());
+console.log(anotherDate.toDateString());
+console.log(anotherDate.toLocaleString());
+
+
+
